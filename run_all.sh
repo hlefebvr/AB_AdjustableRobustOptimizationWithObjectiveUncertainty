50de1419337161503e5ee3111d66312d47695c85
Henri Lefebvre <hri.lefebvre@gmail.com>
2023-04-03 16:48:36 +0200

    add header

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,8 @@
 EXECUTABLE=./cmake-build-debug/FLP/FLP
 INSTANCES=./FLP/data/
 
+echo "result,instance,uncertainty_type,uncertainty_param,objective_type,static_ub,objective,status,reason,n_created_nodes,n_solved_nodes,n_columns,rel_gap,static_adjust_gap,total_time,rmp_time,pricing_time"
+
 for FILE in $INSTANCES/*
 do
   echo "# At $(date), running instance ${FILE}." >> history.txt
